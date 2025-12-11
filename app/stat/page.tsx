08ca24a2373da8ce7c9fe7351fe9164080da3a4d
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2025-12-11 00:49:10 -0800

    Delete page.tsx

--- app/stat/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿export default function StatPage() {
-  return (
-    <section>
-      <h1>STAT</h1>
-      <p>Strength: 5 | Perception: 6 | Endurance: 4</p>
-      <p>Charisma: 7 | Intelligence: 8 | Agility: 6 | Luck: 5</p>
-    </section>
-  );
-}
