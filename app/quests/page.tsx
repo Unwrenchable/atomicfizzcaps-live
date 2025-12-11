72d0bb5e5946d34d4c7031946e2eef18309a2a50
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2025-12-11 00:47:41 -0800

    Delete page.tsx

--- app/quests/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿export default function QuestsPage() {
-  return (
-    <section>
-      <h1>QUESTS</h1>
-      <p>☑ Restore the Mojave Radio Tower</p>
-      <p>☐ Collect 10 Atomic Fizzcaps</p>
-    </section>
-  );
-}
