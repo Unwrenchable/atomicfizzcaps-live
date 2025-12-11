41bbdac0f5bb932884b3be2712ecea29e1ad6120
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2025-12-11 00:46:34 -0800

    Delete page.tsx

--- app/map/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function MapPage() {
-  return (
-    <section>
-      <h1>MAP</h1>
-      <p>This is the MAP tab. Your old homepage is now the default at /</p>
-    </section>
-  );
-}
