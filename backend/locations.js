798304433b483c201c830a28013ad479deb0c597
Unwrenchable <78015550+Unwrenchable@users.noreply.github.com>
2025-12-11 23:53:47 -0800

    Delete backend directory

--- backend/locations.js
+++
@@ -1,7 +0,0 @@
-﻿import locations from '../../data/locations.json';
-
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
-  res.status(200).json(locations);
-}
